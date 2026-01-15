7dca08e767591ad69651f5551e8f7888fee7a7c5
laziz0609 <laziz20060927@gmail.com>
2026-01-15 18:07:27 +0500

    tasklar bajarildi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,25 @@
+import csv
 
+input_path = "students.csv"
+output_path = "rating.csv"
+
+
+with open(input_path) as f:
+    reader = csv.DictReader(f, fieldnames=["name", "score"])
+    next(reader)
+    
+    data = list(reader)
+    
+    data = sorted(data, key=lambda item: item["score"], reverse=True)
+    
+    for i in range(len(data)):
+        data[i]['sn'] = i + 1
+        
+
+with open(output_path, "w", newline="") as f:
+    writer = csv.DictWriter(f, fieldnames=["sn", "name", "score"])
+    
+    writer.writeheader()
+
+    # Write all the data rows
+    writer.writerows(data)
